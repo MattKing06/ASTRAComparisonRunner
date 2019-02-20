f77bc759a3f84d0c1d6b60596796236c4b9a6e05
Matthew King <matthew.king@stfc.ac.uk>
2019-02-20 14:01:20 +0000

    we can now grab the text from each LineEdit object, these strings are then put into a python list ready to be piped as command line arguments

--- astra_runner.py
+++ astra_runner.py
@@ -11,30 +11,56 @@ class MyWindow(QMainWindow):
 		self.ui = ui_main_window()
 		self.ui.setupUi(self)
 		self.ui.runButton.clicked.connect(self.run_astra)
+		self.parameters = []
 		self.show()
-	
-	def setParameterLabels(parameter_labels, self):
+
+	def run_astra(parameters, self):
+	# parameter values will be in a list gathered from
+	# the QLineEdits due to input into ASTRA command
+		parameters = astra_runner_window.collect_parameters()
+		print astra_runner_window.parameters
+		astra_runner_window.check_parameters(parameters)
+		#code to set up ASTRA command
+		print 'RUN ASTRA CLICKED'
+		last_run_parameters = parameters
+		astra_runner_window.parameters = []
+
+	def set_parameter_labels(parameter_labels, self):
 	# Instead of hardcoding the parameter labels in the .ui file
 	# we can just define a list of parameter strings here and set
 	# the labels to them via python.
-	
+		print 'setParameterLabels method'
+
 	def collect_parameters(self):
-	# here we can just grab all of the inputs from the QLineEdits
-	# and put them into a list to be passed to 'run_astra' function
-	
+		#Grab all of the formLayouts that are in the grid layout
+		#we have 4 of them in one gridlayout
+		form_layout_list = self.ui.gridLayout.children()
+		
+		#go through the forms and get all of the children (QWidgets) that 
+		# are contained within that form
+		for form in form_layout_list:
+			number_of_elements = form.count()
+			for i in range(0,number_of_elements):
+				#if we have a QLineEdit then put its text into the parameter list
+				 if isinstance(form.itemAt(i).widget(), QtGui.QLineEdit):
+					 astra_runner_window.parameters.append(str(form.itemAt(i).widget().text()))
+				# if we have a QLabel then we just pass over it (we can maybe do something with this)
+				 elif isinstance(form.itemAt(i).widget(), QtGui.QLabel):
+					pass
+				# if we have neither, we have clearly gone wrong somewhere
+				# so just output an error message to cmd-line (could be a splash screen), and exit the GUI
+				 else:
+					print "Cannot Access GUI Component, please inform a developer."
+					sys.exit(0)
+
 	def check_parameters(parameters, self):
-	# here we will check that the values of the parameters are 
+	# here we will check that the values of the parameters are
 	# of the correct type for the ASTRA command
 	#    1) Needs to be of correct type
 	#    2) Needs to be of realistic values
 	#    3) ????
-	
-	def run_astra(parameters, self):
-	# parameter values will be in a list gathered from
-	# the QLineEdits due to input into ASTRA command
-		print 'RUN ASTRA CLICKED'
-		
-		
+		print 'check_parameters method'
+
 if __name__ == '__main__':
 	application = QtGui.QApplication(sys.argv)
 	astra_runner_window = MyWindow()
